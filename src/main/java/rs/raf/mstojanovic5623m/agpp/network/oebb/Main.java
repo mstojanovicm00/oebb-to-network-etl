8026635c8a394b0d5d577ae8bb8119292d4cf659
mstojanovicm00 <mstojanovicm00@gmail.com>
2025-06-15 20:05:12 +0200

    Run configuration modified
    Modification included:
    - specifying command line arguments
    - rewriting Main.java so that it uses the command line arguments instead of strings for filenames.

--- src/main/java/rs/raf/mstojanovic5623m/agpp/network/oebb/Main.java
+++ src/main/java/rs/raf/mstojanovic5623m/agpp/network/oebb/Main.java
@@ -2,12 +2,8 @@ package rs.raf.mstojanovic5623m.agpp.network.oebb;
 
 public class Main {
     public static void main(String[] args) {
-        String in = "/home/mihajlo-stojanovic/Faculty/Master/Usmerenja/" +
-                "Automatsko generisanje plana putovanja/00 Podaci/" +
-                "GTFS_OP_2025_obb OEBB";
-        String out = "/home/mihajlo-stojanovic/Faculty/Master/Usmerenja/" +
-                "Automatsko generisanje plana putovanja/00 Podaci/Warehouse/" +
-                "network";
+        String in = args[0];
+        String out = args[1];
         Launcher.getLauncher().launch(in, out);
     }
 }
